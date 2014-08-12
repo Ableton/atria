8d0c6376152b31289987a6d5380ce88cf5ccdc1c
jbo <jbo@ableton.com>
2014-08-12 17:31:36 +0200

    base: extract CommonType from variant details into meta

--- src/ableton/base/variant/Visitor.hpp
+++ src/ableton/base/variant/Visitor.hpp
@@ -2,13 +2,10 @@
 
 #pragma once
 
+#include <ableton/base/meta/CommonType.hpp>
+
 #include <boost/variant/static_visitor.hpp>
-#include <boost/mpl/eval_if.hpp>
-#include <boost/mpl/and.hpp>
-#include <boost/mpl/not.hpp>
-#include <boost/mpl/vector.hpp>
-#include <boost/mpl/fold.hpp>
-#include <boost/mpl/identity.hpp>
+
 #include <type_traits>
 #include <cassert>
 #include <utility>
@@ -51,8 +48,6 @@ private:
   detail::VisitorImpl<Fns...> mImpl;
 };
 
-
-
 namespace detail
 {
 
@@ -66,67 +61,6 @@ struct Bottom
   template <typename T> operator T&();
   template <typename T> operator const T&();
 };
-
-namespace mpl = boost::mpl;
-
-//!
-// Removes r-value reference from type T if is not present in any of
-// the types in `OrigT`.  This allows to remove unexpected r-references
-// from a type that were added because of the use of `declval`.
-//
-template<class ValT, class ...OrigTs>
-struct Undeclval
-  : mpl::eval_if<mpl::and_<std::is_rvalue_reference<ValT>,
-                           mpl::not_<std::is_rvalue_reference<OrigTs> >... >,
-                 std::remove_reference<ValT>,
-                 mpl::identity<ValT> >
-{};
-
-template <typename ...Ts>
-struct CouldNotFindCommonType
-{
-  template <typename T>
-  CouldNotFindCommonType(T&&) {};
-};
-
-template <typename T, typename U, typename Enable=void>
-struct CommonType2
-{
-  using type = CouldNotFindCommonType<T, U>;
-};
-
-template <typename T, typename U>
-struct CommonType2<
-  T, U,
-  typename std::conditional<
-    true, void, decltype(true ? std::declval<T>() : std::declval<U>())
-    >::type >
-{
-  using type = typename Undeclval<
-    decltype(true ? std::declval<T>() : std::declval<U>()),
-    T, U
-  >::type;
-};
-
-//!
-// Similar to `std::common_type` but addresses several issues.  First,
-// on Clang 3.4, `common_type` fails for `void`, where it should not.
-// Also, the standard common type removes qualification, which we want
-// to preserve. Also, `common_type` is SFINAE-friendly only in new
-// versions of GCC.
-//
-// This implementation preserves qualification when possible, and also
-// is total. When no common type is found, it returns the special type
-// `CouldNotFindCommonType`, which can be instantiated and converted
-// from anything.  This makes it easier to write functions that return
-// a common-type of other types, but that might be used in expressions
-// where the return type is to be discarded.  This erroneous type was
-// chosen instead of `void` to make debugging easier.
-//
-template <typename T, typename ...Ts>
-struct CommonType
-  : mpl::fold<mpl::vector<Ts...>, T, CommonType2<mpl::_1, mpl::_2> >
-{};
 
 } // namespace detail
 
@@ -218,7 +152,7 @@ when(Fn&& fn)
 //
 template <typename... Fns>
 Visitor<
-  typename detail::CommonType<
+  typename meta::CommonType<
       typename std::result_of<Fns(detail::Bottom)>::type...
     >::type,
   Fns...>
