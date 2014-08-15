bd88eba5cebca6c1be59c3379c6ec6ededa44213
jbo <jbo@ableton.com>
2014-08-15 11:28:40 +0200

    funken: add minimal support for Clojure style transducers

--- src/ableton/funken/Transducers.hpp
+++ src/ableton/funken/Transducers.hpp
@@ -3,9 +3,11 @@
 #pragma once
 
 #include <ableton/estd/type_traits.hpp>
+#include <ableton/estd/utility.hpp>
 #include <boost/fusion/adapted/std_tuple.hpp>
 #include <boost/fusion/include/reverse_fold.hpp>
 #include <tuple>
+#include <numeric>
 
 namespace ableton {
 namespace funken {
@@ -52,6 +54,139 @@ detail::Composed<Fn, Fns...> comp(Fn&& fn, Fns&& ...fns)
 {
   return { std::forward<Fn>(fn), std::forward<Fns>(fns)... };
 };
+
+namespace detail {
+
+//!
+// Utility to write simple transducers easily.  Most transducers have
+// the following structure:
+//
+// @code{.hs}
+//  xducer params... =     -- 1. this returns a transducer
+//    \ reducer ->         -- 2. this is a transducer
+//       \ state, input -> -- 3. this is transformed version of 'reducer'
+//           f(params..., reducer, state, input)
+// @endocde
+//
+// Writing functions that return functions without erasing their type
+// is very cumbersome in C++11 (this is not true with decltype(auto)
+// in the next standard).  The `Transducer` type implements the first
+// two calls. The `ResultReducerT` parameter should implement the last
+// call.  It will be constructed `reducer, params...` and it should be
+// callable with `state, input`.
+//
+// @see map
+// @see filter
+//
+template<template <class, class...> class ResultReducerT,
+         typename ...ParamTs>
+struct Transducer : std::tuple<ParamTs...>
+{
+  using std::tuple<ParamTs...>::tuple;
+
+  template<typename ReducerT>
+  auto operator() (ReducerT&& reducer) const
+    -> ResultReducerT<ReducerT, ParamTs...>
+  {
+    using Indexes = estd::make_index_sequence<sizeof...(ParamTs)>;
+    return this->make(std::forward<ReducerT>(reducer), Indexes());
+  }
+
+  template<typename ReducerT, std::size_t...Indexes>
+  auto make(ReducerT&& reducer, estd::index_sequence<Indexes...>) const
+    -> ResultReducerT<ReducerT, ParamTs...>
+  {
+    return { std::forward<ReducerT>(reducer),
+             std::get<Indexes>(*this)... };
+  }
+};
+
+template <typename ReducerT,
+          typename MappingT>
+struct MapReducer
+{
+  ReducerT reducer;
+  MappingT mapping;
+
+  template <typename State, typename Input>
+  auto operator() (State&& s, Input&& i) const
+    -> decltype(reducer(s, mapping(i)))
+  {
+    return reducer(s, mapping(i));
+  }
+};
+
+template <typename ReducerT,
+          typename PredicateT>
+struct FilterReducer
+{
+  ReducerT reducer;
+  PredicateT predicate;
+
+  template <typename State, typename Input>
+  auto operator() (State&& s, Input&& i) const
+    -> decltype(predicate(i) ? reducer(s, i) : s)
+  {
+    return predicate(i) ? reducer(s, i) : s;
+  }
+};
+
+} // namespace detail
+
+//!
+// Similar to clojure.core/map$1
+//
+template <typename MappingT>
+auto map(MappingT&& mapping)
+  -> detail::Transducer<detail::MapReducer, MappingT>
+{
+  return detail::Transducer<detail::MapReducer, MappingT> {
+    std::forward<MappingT>(mapping) };
+}
+
+//!
+// Similar to clojure.core/filter$1
+//
+template <typename PredicateT>
+auto filter(PredicateT&& predicate)
+  -> detail::Transducer<detail::FilterReducer, PredicateT>
+{
+  return detail::Transducer<detail::FilterReducer, PredicateT> {
+    std::forward<PredicateT>(predicate) };
+}
+
+//!
+// Similar to clojure.core/identity
+//
+constexpr struct Identity
+{
+  template <typename FirstT, typename ...RestT>
+  constexpr auto operator() (FirstT&& x, RestT&&...) const
+    -> decltype(std::forward<FirstT>(x))
+  {
+    return std::forward<FirstT>(x);
+  }
+} identity {};
+
+
+//!
+// Similar to clojure.core/transduce, but with parameter ordering more
+// similar to those convetional in the C++ standard library.
+//
+template <typename InputRangeT,
+          typename XformT,
+          typename ReducerT,
+          typename StateT>
+StateT transduce(InputRangeT&& range, XformT&& xform,
+                 ReducerT&& reducer, StateT&& state)
+{
+  auto xformed = xform(reducer);
+  return std::accumulate(
+    std::begin(range),
+    std::end(range),
+    state,
+    xformed);
+}
 
 } // namespace funken
 } // namespace ableton
--- src/ableton/funken/tst_Transducers.cpp
+++ src/ableton/funken/tst_Transducers.cpp
@@ -43,5 +43,41 @@ TEST(Compose, VariousTypes)
   EXPECT_EQ(fn(42), 43.0);
 }
 
+TEST(Transduce, Identity)
+{
+  auto v = std::vector<int> { 1, 2, 3, 6 };
+  EXPECT_EQ(transduce(v, identity, std::plus<int>{}, 1), 13);
+}
+
+TEST(Transduce, Mapping)
+{
+  auto v = std::vector<int> { 1, 2, 3, 6 };
+  auto times2 = [] (int x) { return x * 2; };
+  EXPECT_EQ(transduce(v, map(times2), std::plus<int>{}, 1), 25);
+}
+
+TEST(Transduce, Filter)
+{
+  auto v = std::vector<int> { 1, 2, 3, 6 };
+  auto odd = [] (int x) { return x % 2 == 0; };
+  EXPECT_EQ(transduce(v, filter(odd), std::plus<int>{}, 1), 9);
+}
+
+TEST(Transduce, Composition)
+{
+  auto v = std::vector<int> { 1, 2, 3, 6 };
+  auto times2 = [] (int x) { return x * 2; };
+  auto odd = [] (int x) { return x % 2 == 0; };
+
+  // Transducers compose from left to right, this is equivalent to
+  // Haskell-like expression:
+  //
+  //   foldl (+) $ map times2 $ filter odd $ v
+  //
+  auto res = transduce(
+    v, comp(filter(odd), map(times2)), std::plus<int>{}, 1);
+  EXPECT_EQ(res, 17);
+}
+
 } // namespace funken
 } // namespace ableton
