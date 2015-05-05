edd9bdc6d6e6c3626c4d3417231e2e7c4e240298
Juan Pedro Bolívar Puente <jbo@ableton.com>
2015-05-05 10:33:55 +0200

    funken: fix Clang does not like our use of declval
    In case you are wondering why this `declval_` clone of `std::declval` is
    needed at all... Clang is too cealous and it statica asserts too much
    inside `declval` to make sure that you don't call it in an evaluated
    context.
    
    In particular, we use declval here inside function default arguments.
    You are crazy Juanpe!  Don't worry, this function is never called, so it
    is not a problem.  But for Clang it is, hence this clone.  Ideally I
    would have never defined it, but then clang warns about a function with
    internal linkage not being defined.

--- src/atria/funken/concepts.hpp
+++ src/atria/funken/concepts.hpp
@@ -1,13 +1,21 @@
-// Copyright: 2014, Ableton AG, Berlin. All rights reserved.
+// Copyright: 2014, 2015, Ableton AG, Berlin. All rights reserved.
 
 #pragma once
 
 #include <atria/meta/concept.hpp>
 #include <atria/estd/type_traits.hpp>
 #include <utility>
+#include <cassert>
 
 namespace atria {
 namespace funken {
+
+template <typename T>
+auto declval_() -> typename std::add_rvalue_reference<T>::type
+{
+  assert(!"You should never run this function!");
+  return *(estd::decay_t<T>*)(nullptr);
+}
 
 //!
 // Concept for objects that provide values via a getter.
@@ -18,7 +26,7 @@ struct In_value
 {
   template <typename T,
             typename V = estd::decay_t<estd::Value_type<T> > >
-  auto requires(T&& x, V& v = std::declval<V&>()) -> decltype(
+  auto requires(T&& x, V& v = declval_<V&>()) -> decltype(
     meta::expressions(
       v = x.get(),
       v = static_cast<const T&&>(x).get()));
@@ -33,7 +41,7 @@ struct Out_value
 {
   template <typename T,
             typename V = estd::Value_type<T> >
-  auto requires(T&& x, V& v = std::declval<V&>()) -> decltype(
+  auto requires(T&& x, V& v = declval_<V&>()) -> decltype(
     meta::expressions(
       (x.set(v), meta::can_be_void),
       (x.set(static_cast<const V&&>(v)), meta::can_be_void)));
