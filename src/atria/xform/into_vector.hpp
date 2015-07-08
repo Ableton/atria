a94fd17dfdf1942f79a5e4eb4af042f312f2f610
jbo <jbo@ableton.com>
2015-07-08 20:33:55 +0200

    xform: add `into_vector` a-la Clojure `into-array`

---
+++ src/atria/xform/into_vector.hpp
@@ -0,0 +1,53 @@
+// Copyright: 2014, 2015, Ableton AG, Berlin. All rights reserved.
+
+#pragma once
+
+#include <atria/xform/into.hpp>
+#include <atria/xform/state_traits.hpp>
+#include <atria/xform/reducing/last_rf.hpp>
+#include <vector>
+
+namespace atria {
+namespace xform {
+
+namespace detail {
+
+template <typename XformT, typename... InputRangeTs>
+struct into_vector_result
+{
+  struct anything_t
+  {
+    template <typename T> operator T&();
+    template <typename T> operator const T&();
+  };
+
+  using xformed_t = decltype(
+    state_complete(
+      std::declval<XformT>()(last_rf)(
+        std::declval<anything_t>(),
+        std::declval<estd::Value_type<InputRangeTs> >()...)));
+
+  using type = std::vector<estd::decay_t<xformed_t> >;
+};
+
+template <typename XformT, typename... InputRangeTs>
+using into_vector_result_t = typename
+  into_vector_result<XformT, InputRangeTs...>::type;
+
+} // namespace detail
+
+/*!
+ * Similar to clojure.core/into-array
+ */
+template <typename XformT,
+          typename ...InputRangeTs>
+auto into_vector(XformT&& xform, InputRangeTs&& ...ranges)
+  -> detail::into_vector_result_t<XformT, InputRangeTs...>
+{
+  return into(detail::into_vector_result_t<XformT, InputRangeTs...>{},
+              std::forward<XformT>(xform),
+              std::forward<InputRangeTs>(ranges)...);
+}
+
+} // namespace xform
+} // namespace atria
---
+++ src/atria/xform/tst_into_vector.cpp
@@ -0,0 +1,66 @@
+// Copyright: 2014, 2015, Ableton AG, Berlin. All rights reserved.
+
+#include <atria/xform/into_vector.hpp>
+#include <atria/xform/transducer/map.hpp>
+#include <atria/xform/transducer/filter.hpp>
+#include <atria/xform/functional.hpp>
+#include <atria/estd/functional.hpp>
+
+#include <atria/testing/gtest.hpp>
+
+namespace atria {
+namespace xform {
+
+namespace test_detail {
+
+struct to_string_t { std::string operator() (int x); };
+struct stoi_t { int operator() (std::string x); };
+
+} // namespace test_detail
+
+TEST(into_vector, type_deduction)
+{
+  using namespace test_detail;
+
+  static_assert(
+    std::is_same<std::vector<int>,
+                 decltype(into_vector(identity,
+                                      std::vector<int>{}))>{}, "");
+  static_assert(
+    std::is_same<std::vector<std::tuple<int, std::string>>,
+                 decltype(into_vector(identity,
+                                      std::vector<int>{},
+                                      std::vector<std::string>{}))>{}, "");
+  static_assert(
+    std::is_same<std::vector<std::string>,
+                 decltype(into_vector(map(to_string_t{}),
+                                      std::vector<int>{}))>{}, "");
+  static_assert(
+    std::is_same<std::vector<int>,
+                 decltype(into_vector(map(stoi_t{}),
+                                      std::vector<std::string>{}))>{}, "");
+}
+
+TEST(into_vector, transduction)
+{
+  auto v = std::vector<int> { 1, 2, 3, 4 };
+
+  auto xform = comp(
+    filter([] (int x) { return x % 2 == 0; }),
+    map([] (int x) { return std::to_string(x); }));
+  auto res = into_vector(xform, v);
+  EXPECT_EQ(res, (std::vector<std::string> { "2", "4" }));
+}
+
+TEST(into_vector, zipping)
+{
+  auto v1 = std::vector<int> { 1, 2, 3, 4 };
+  auto v2 = std::vector<std::string> { "a", "b" };
+  using tup = std::tuple<int, std::string>;
+
+  auto res = into_vector(identity, v1, v2);
+  EXPECT_EQ(res, (std::vector<tup> { tup(1, "a"), tup(2, "b") }));
+}
+
+} // namespace xform
+} // namespace atria
