b48eee2b925211b53c1dceb8e8fe7d5f206f51e0
Juan Pedro Bolívar Puente <raskolnikov@gnu.org>
2015-08-14 11:04:23 +0200

    meta: add lazy version of `enable_if`

---
+++ src/atria/meta/lazy_enable_if.hpp
@@ -0,0 +1,22 @@
+// Copyright: 2014, 2015, Ableton AG, Berlin. All rights reserved.
+
+#pragma once
+
+#include <atria/meta/eval.hpp>
+#include <utility>
+
+namespace atria {
+namespace meta {
+
+namespace detail {
+struct disabled {};
+} // namespace detail
+
+template <bool C, typename T>
+struct lazy_enable_if : estd::conditional_t<C, T, detail::disabled> {};
+
+template <bool C, typename T>
+using lazy_enable_if_t = meta::eval_t<lazy_enable_if<C, T> >;
+
+} // namespace meta
+} // namespace atria
