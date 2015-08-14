90c06a21c07dea4b8efca1b603f258669b407511
Juan Pedro Bolívar Puente <raskolnikov@gnu.org>
2015-08-14 12:11:19 +0200

    xform: add `drop` transducer, a-la Clojure

---
+++ src/atria/xform/transducer/drop.hpp
@@ -0,0 +1,65 @@
+// Copyright: 2014, 2015, Ableton AG, Berlin. All rights reserved.
+
+#pragma once
+
+#include <atria/xform/transducer_impl.hpp>
+#include <atria/xform/state_wrapper.hpp>
+#include <atria/xform/skip.hpp>
+#include <atria/prelude/constantly.hpp>
+
+namespace atria {
+namespace xform {
+
+namespace detail {
+
+struct drop_rf_gen
+{
+  template <typename ReducingFnT,
+            typename IntegralT>
+  struct apply
+  {
+    ReducingFnT step;
+    IntegralT total;
+
+    template <typename StateT, typename ...InputTs>
+    auto operator() (StateT&& s, InputTs&& ...is)
+      -> decltype(
+        wrap_state(call(step, state_unwrap(std::forward<StateT>(s)),
+                        std::forward<InputTs>(is)...),
+                   total))
+    {
+      auto count = state_data(std::forward<StateT>(s),
+                              constantly(total));
+      return count != 0
+        ? wrap_state(
+          skip(step,
+               state_unwrap(std::forward<StateT>(s)),
+               std::forward<InputTs>(is)...),
+          count - 1)
+        : wrap_state(
+          call(step,
+               state_unwrap(std::forward<StateT>(s)),
+               std::forward<InputTs>(is)...),
+          count);
+    }
+  };
+};
+
+} // namespace detail
+
+template <typename T>
+using drop_t = transducer_impl<detail::drop_rf_gen, T>;
+
+/*!
+ * Similar to clojure.core/drop$1
+ */
+template <typename IntegralT>
+auto drop(IntegralT&& n)
+  -> drop_t<estd::decay_t<IntegralT> >
+{
+  return drop_t<estd::decay_t<IntegralT> > {
+    std::forward<IntegralT>(n) };
+}
+
+} // namespace xform
+} // namespace atria
---
+++ src/atria/xform/transducer/tst_drop.cpp
@@ -0,0 +1,51 @@
+// Copyright: 2014, 2015, Ableton AG, Berlin. All rights reserved.
+
+#include <atria/xform/concepts.hpp>
+#include <atria/xform/into_vector.hpp>
+#include <atria/xform/transducer/drop.hpp>
+#include <atria/xform/transducer/take.hpp>
+#include <atria/prelude/comp.hpp>
+#include <atria/testing/gtest.hpp>
+#include <atria/estd/functional.hpp>
+
+namespace atria {
+namespace xform {
+
+TEST(drop, concept)
+{
+  using namespace std::placeholders;
+  meta::check<Transparent_transducer_spec(
+   decltype(drop(std::bind(estd::less<>{}, 42, _1))))>();
+}
+
+TEST(drop, into)
+{
+  using namespace std::placeholders;
+  auto v = std::vector<int> { 1, 2, 3, 4, 5 };
+
+  {
+    auto res = into_vector(drop(0), v);
+    EXPECT_EQ(res, (decltype(res) { 1, 2, 3, 4, 5 }));
+  }
+
+  {
+    auto res = into_vector(drop(2), v);
+    EXPECT_EQ(res, (decltype(res) { 3, 4, 5 }));
+  }
+
+  {
+    auto res = into_vector(drop(6), v);
+    EXPECT_EQ(res, (decltype(res) {}));
+  }
+}
+
+TEST(drop, compose)
+{
+  using namespace std::placeholders;
+  auto v = std::vector<int> { 1, 2, 3, 4, 5 };
+  auto res = into_vector(comp(drop(2), take(2)), v);
+  EXPECT_EQ(res, (decltype(res) { 3, 4 }));
+}
+
+} // namespace xform
+} // namespace atria
