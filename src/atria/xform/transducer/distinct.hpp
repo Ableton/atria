99785c3d0be44726b137553d50341a9a8829ac83
Juan Pedro Bolívar Puente <raskolnikov@gnu.org>
2015-08-15 18:22:03 +0200

    xform: more performance `distinct` avoiding union types

--- src/atria/xform/transducer/distinct.hpp
+++ src/atria/xform/transducer/distinct.hpp
@@ -7,10 +7,11 @@
 
 #include <atria/xform/transducer_impl.hpp>
 #include <atria/xform/state_wrapper.hpp>
-#include <atria/xform/skip.hpp>
+#include <atria/xform/with_state.hpp>
 #include <atria/estd/tuple_hash.hpp>
 #include <atria/prelude/tuplify.hpp>
 #include <atria/estd/functional.hpp>
+#include <atria/meta/copy_traits.hpp>
 #include <unordered_set>
 
 namespace atria {
@@ -27,32 +28,36 @@ struct distinct_rf_gen
 
     template <typename ...InputTs>
     using cache_t = std::unordered_set<
-        estd::decay_t<
-          decltype(tuplify(std::declval<InputTs>()...))> >;
+        estd::decay_t<decltype(tuplify(std::declval<InputTs>()...))> >;
 
     template <typename StateT, typename ...InputTs>
     auto operator() (StateT&& s, InputTs&& ...is)
-      -> decltype(
-        wrap_state(
-          call(step,
-               state_unwrap(std::forward<StateT>(s)),
-               std::forward<InputTs>(is)...),
-          cache_t<InputTs...>{}))
+      -> decltype(wrap_state(step(state_unwrap(s), is...), cache_t<InputTs...>{}))
     {
-      auto cache = state_data(std::forward<StateT>(s), []{
-          return cache_t<InputTs...>{};
-        });
+      using result_t   = decltype(wrap_state(step(state_unwrap(s), is...), cache_t<InputTs...>{}));
+      using complete_t = decltype(state_complete(s));
 
-      auto unique = cache.insert(tuplify(is...)).second;
-      return wrap_state(
-          unique
-            ? call(step,
-                   state_unwrap(std::forward<StateT>(s)),
-                   std::forward<InputTs>(is)...)
-            : skip(step,
-                   state_unwrap(std::forward<StateT>(s)),
+      using wrapped_t   = meta::copy_decay_t<StateT, estd::decay_t<result_t>>;
+      using unwrapped_t = meta::copy_decay_t<StateT, estd::decay_t<complete_t>>;
+
+      return with_state(
+        std::forward<StateT>(s),
+        [&] (unwrapped_t&& st) {
+          return wrap_state(
+            step(state_unwrap(std::forward<unwrapped_t>(st)),
+                 std::forward<InputTs>(is)...),
+            cache_t<InputTs...>{ tuplify(is...) });
+        },
+        [&] (wrapped_t st) {
+          auto& cache = state_wrapper_data(st);
+          auto unique = cache.insert(tuplify(is...)).second;
+          return !unique
+            ? st
+            : wrap_state(
+              step(std::move(state_unwrap(st)),
                    std::forward<InputTs>(is)...),
-          std::move(cache));
+              std::move(cache));
+        });
     }
   };
 };
